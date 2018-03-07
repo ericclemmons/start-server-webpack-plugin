1f6e66459977d17136d01feca60e619e26a7b6f0
Wout Mertens <Wout.Mertens@gmail.com>
2018-03-07 23:15:55 +0100

    Use childProcess.fork()
    * use process messaging to communicate
    * check for loaded before restarting
    * needs `start-server-webpack-plugin/dist/monitor` to be loaded last in the entry
    * make sure not to mark monitor as external, needs to be built with webpack

--- src/StartServerPlugin.js
+++ src/StartServerPlugin.js
@@ -1,4 +1,4 @@
-import cluster from 'cluster';
+import childProcess from 'child_process';
 
 export default class StartServerPlugin {
   constructor(options) {
@@ -19,6 +19,9 @@ export default class StartServerPlugin {
     this.afterEmit = this.afterEmit.bind(this);
     this.apply = this.apply.bind(this);
     this.startServer = this.startServer.bind(this);
+    this._handleChildError = this._handleChildError.bind(this);
+    this._handleChildExit = this._handleChildExit.bind(this);
+    this._handleChildMessage = this._handleChildMessage.bind(this);
 
     this.worker = null;
     if (this.options.restartable !== false) {
@@ -44,38 +47,61 @@ export default class StartServerPlugin {
   _getArgs() {
     const {options} = this;
     const execArgv = (options.nodeArgs || []).concat(process.execArgv);
-    if (options.args) {
-      execArgv.push('--');
-      execArgv.push.apply(execArgv, options.args);
-    }
     return execArgv;
   }
 
-  _getInspectPort(execArgv) {
-    const inspectArg = execArgv.find(arg => arg.includes('--inspect'));
-    if (!inspectArg || !inspectArg.includes('=')) {
+  _handleChildExit(code, signal) {
+    if (code) console.error('sswp> script exited with code', code);
+    if (signal) console.error('sswp> script exited after signal', signal);
+
+    this.worker = null;
+
+    if (!this.workerLoaded) {
+      console.error('sswp> Script did not load, not restarting');
       return;
     }
-    const hostPort = inspectArg.split('=')[1];
-    const port = hostPort.includes(':') ? hostPort.split(':')[1] : hostPort;
-    return parseInt(port);
+
+    this.workerLoaded = false;
+    this._runWorker();
   }
 
-  _getSignal() {
-    const {signal} = this.options;
-    // allow users to disable sending a signal by setting to `false`...
-    if (signal === false) return;
-    if (signal === true) return 'SIGUSR2';
-    return signal;
+  _handleChildError(err) {
+    this.worker = null;
+  }
+
+  _handleChildMessage(message) {
+    console.warn('sswp> got', message);
+    if (message === 'SSWP_LOADED') {
+      this.workerLoaded = true;
+    } else if (message === 'SSWP_HMR_FAIL') {
+      this.workerLoaded = false;
+    }
+  }
+
+  _runWorker(callback) {
+    const {existsAt, execArgv, options, worker} = this;
+    if (worker) return;
+
+    console.warn('sswp> running script');
+    this.worker = childProcess.fork(existsAt, options.args, {execArgv});
+    this.worker.once('exit', this._handleChildExit);
+    this.worker.once('error', this._handleChildError);
+    this.worker.on('message', this._handleChildMessage);
+
+    if (callback) callback();
+  }
+
+  _hmrWorker(compilation, callback) {
+    if (this.worker && this.worker.send) {
+      console.warn('sswp> notifying worker');
+      this.worker.send('SSWP_HMR');
+    }
+    callback();
   }
 
   afterEmit(compilation, callback) {
-    if (this.worker && this.worker.isConnected()) {
-      const signal = this._getSignal();
-      if (signal) {
-        process.kill(this.worker.process.pid, signal);
-      }
-      return callback();
+    if (this.worker) {
+      return this._hmrWorker(compilation, callback);
     }
 
     this.startServer(compilation, callback);
@@ -115,33 +141,10 @@ export default class StartServerPlugin {
       }
     }
     const {existsAt} = compilation.assets[name];
-    this._entryPoint = existsAt;
-
-    this._startServer(worker => {
-      this.worker = worker;
-      callback();
-    });
-  }
-
-  _startServer(callback) {
     const execArgv = this._getArgs();
-    const inspectPort = this._getInspectPort(execArgv);
-
-    const clusterOptions = {
-      exec: this._entryPoint,
-      execArgv,
-    };
-
-    if (inspectPort) {
-      clusterOptions.inspectPort = inspectPort;
-    }
-    cluster.setupMaster(clusterOptions);
-
-    cluster.on('online', worker => {
-      callback(worker);
-    });
-
-    cluster.fork();
+    this.existsAt = existsAt;
+    this.execArgv = execArgv;
+    this._runWorker(callback);
   }
 }
 
