1fa02967f7bf466828680303186ed155065c8b5f
Wout Mertens <Wout.Mertens@gmail.com>
2018-03-07 23:38:58 +0100

    Fix tests
    Feels a bit silly have to install webpack just to pass the tests, but maybe we can do better tests later.
    
    Right now we don't test the instrumenting nor the running. Maybe we can test that end-to-end.

--- test/StartServerPlugin.test.js
+++ test/StartServerPlugin.test.js
@@ -10,9 +10,9 @@ describe('StartServerPlugin', function() {
     expect(require('..')).toBe(Plugin);
   });
 
-  it('should accept a string name', function() {
+  it('should accept a string entryName', function() {
     const p = new Plugin('test');
-    expect(p.options.name).toBe('test');
+    expect(p.entryName).toBe('test');
   });
 
   it('should accept an options object', function() {
@@ -23,55 +23,6 @@ describe('StartServerPlugin', function() {
   it('should calculate arguments', function() {
     const p = new Plugin({nodeArgs: ['meep'], args: ['moop']});
     const args = p._getArgs();
-    expect(args.filter(a => a === 'meep').length).toBe(1);
-    expect(args.slice(-2)).toEqual(['--', 'moop']);
-  });
-
-  it('should parse the inspect port', function() {
-    const p = new Plugin({nodeArgs: ['--inspect=9230']});
-    const port = p._getInspectPort(p._getArgs());
-    expect(port).toBe(9230);
-  });
-
-  it('should remove host when parsing inspect port', function() {
-    const p = new Plugin({nodeArgs: ['--inspect=localhost:9230']});
-    const port = p._getInspectPort(p._getArgs());
-    expect(port).toBe(9230);
-  });
-
-  it('should return undefined inspect port is not set', function() {
-    const p = new Plugin({nodeArgs: ['--inspect']});
-    const port = p._getInspectPort(p._getArgs());
-    expect(port).toBe(undefined);
-  });
-
-  it('should not use signal if signal is not passed', function() {
-    const p = new Plugin();
-    const signal = p._getSignal();
-    expect(signal).toBe(undefined);
-  });
-
-  it('should return default signal if signal is true', function() {
-    const p = new Plugin({signal: true});
-    const signal = p._getSignal();
-    expect(signal).toBe('SIGUSR2');
-  });
-
-  it('should allow user to override the default signal', function() {
-    const p = new Plugin({signal: 'SIGUSR1'});
-    const signal = p._getSignal();
-    expect(signal).toBe('SIGUSR1');
-  });
-
-  it('should allow user to override the default signal', function() {
-    const p = new Plugin({signal: 2});
-    const signal = p._getSignal();
-    expect(signal).toBe(2);
-  });
-
-  it('should allow user to disable sending a signal', function() {
-    const p = new Plugin({signal: false});
-    const signal = p._getSignal();
-    expect(signal).toBe(undefined);
+    expect(args).toEqual(['meep']);
   });
 });
